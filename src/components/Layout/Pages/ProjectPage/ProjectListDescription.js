23bc309402455988a2b88efe401bd69ad3778696
Kristopher-Johnson <kristopher.mj@hotmail.com>
2022-01-14 03:41:31 +0000

    Formatted Project List + Added Project Description

---
+++ src/components/Layout/Pages/ProjectPage/ProjectListDescription.js
@@ -0,0 +1,13 @@
+import classes from "./ProjectListDescription.module.css";
+
+const ProjectListDescription = (props) => {
+  return (
+    <div className={classes.section}>
+      <div className={classes.inputs}>
+        <input></input>
+        <input></input>
+      </div>
+    </div>
+  );
+};
+export default ProjectListDescription;
