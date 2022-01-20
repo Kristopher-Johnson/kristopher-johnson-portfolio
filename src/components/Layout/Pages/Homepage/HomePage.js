c66479784999ee4fd63c7699b9a02acd546cbe86
Kristopher-Johnson <kristopher.mj@hotmail.com>
2022-01-20 07:17:49 +0000

    Added some media queries

--- src/components/Layout/Pages/Homepage/HomePage.js
+++ src/components/Layout/Pages/Homepage/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = (props) => {
   return (
     <div className={classes.outer}>
       <div className={classes.content}>
-        <p>This is some content about my page</p>
+        <p>This is some content absout my page</p>
       </div>
     </div>
   );
