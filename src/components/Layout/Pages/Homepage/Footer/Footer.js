3d26aef49bcd1cda9b42844a5fdbf40ecd8dbb4d
Kristopher-Johnson <kristopher.mj@hotmail.com>
2022-01-31 22:27:05 +0000

    fixed bug for text overflow at screen width < 600px project elements

--- src/components/Layout/Pages/Homepage/Footer/Footer.js
+++ src/components/Layout/Pages/Homepage/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className={classes.margin}>
       <h1 className={classes["rainbow-text"]}>
-        Thanks you for reading my portfolio, please feel free to view some of my
+        Thank you for reading my portfolio, please feel free to view some of my
         projects or get in touch on the contact page!
       </h1>
     </div>
